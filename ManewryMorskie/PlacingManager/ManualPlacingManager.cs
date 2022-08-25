4235e8d2700b476b1efffdda23d36c38f26a299e
Mikołaj Makowski <308125@o365.stud.umk.pl>
2022-08-25 20:09:47 +0200

    Add manual batteries placing

--- ManewryMorskie/PlacingManager/ManualPlacingManager.cs
+++ ManewryMorskie/PlacingManager/ManualPlacingManager.cs
@@ -10,6 +10,9 @@ namespace ManewryMorskie.PlacingManagerComponents
 
     public class ManualPlacingManager : PlacingMangerBase, IPlacingManager, IDisposable
     {
+        private static readonly List<CellLocation> topBatteriesPlaces = new() { (1, 16), (1, 15), (3, 16), (8, 14), (10, 14) };
+        private static readonly List<CellLocation> bottomBatteriesPlaces = new() { (1, 3), (3, 3), (8, 1), (10, 1), (10, 2) };
+
         private readonly Dictionary<Type, string> unitsLabels = new();
         private readonly Player currentPlayer;
 
@@ -32,25 +35,35 @@ namespace ManewryMorskie.PlacingManagerComponents
 
         public async Task PlacePawns(CancellationToken token)
         {
-            await PlaceDefaultBatteries(currentPlayer);
-
-            LocationSelectionHandler selectionHandler = new(Ui);
-            OptionsHandler optionsHandler = new(Ui);
-            List<CellLocation>? selectable = default;
+            List<CellLocation> selectable = players.TopPlayer == currentPlayer ? topBatteriesPlaces : bottomBatteriesPlaces;
+            await Place(selectable, kpv => kpv.Key == typeof(Bateria) && kpv.Value != 0,
+                "Rozmieść swoje baterie na planszy", token);
 
             selectable = map.Keys.Where(l => map[l].Owner == currentPlayer && map[l].Unit == null).ToList();
+            await Place(selectable, kpv => kpv.Value != 0, "Rozmieść swoje pionki na planszy", token);
+
+            await currentPlayer.UserInterface.MarkCells(map.Keys, MarkOptions.None);
+            await currentPlayer.UserInterface.DisplayMessage("Zaczekaj aż przeciwnik ustawi pionki", MessageType.SideMessage);
+        }
+
+        private async Task Place(List<CellLocation> selectable, Func<KeyValuePair<Type, int>, bool> selector,
+            string msg, CancellationToken token)
+        {
+            LocationSelectionHandler selectionHandler = new(Ui);
+            OptionsHandler optionsHandler = new(Ui);
+
             await Ui.MarkCells(selectable, MarkOptions.Selectable);
 
             do
             {
-                await Ui.DisplayMessage("Rozmieść swoje pionki na planszy", MessageType.SideMessage);
+                await Ui.DisplayMessage(msg, MessageType.SideMessage);
 
                 CellLocation selected = await selectionHandler.WaitForCorectSelection(selectable, token);
                 await Ui.MarkCells(selected, MarkOptions.Selected);
                 await Ui.DisplayMessage("Wybierz jednostkę jaką chcesz umieścić", MessageType.SideMessage);
 
                 Type? chosenUnitType = await optionsHandler.ChooseOption(
-                    options: unitsToPlace.Where(v => v.Value != 0)
+                    options: unitsToPlace.Where(selector)
                         .Select(vp => new KeyValuePair<string, Type>($"{unitsLabels[vp.Key]} ({vp.Value})", vp.Key))
                         .ToList(),
                      context: selected,
@@ -59,10 +72,7 @@ namespace ManewryMorskie.PlacingManagerComponents
                 await PlaceUnit(selected, chosenUnitType!, currentPlayer);
                 selectable.Remove(selected);
                 await currentPlayer.UserInterface.MarkCells(selected, MarkOptions.None);
-            } while (unitsToPlace.Any(x => x.Value != 0));
-
-            await currentPlayer.UserInterface.MarkCells(map.Keys, MarkOptions.None);
-            await currentPlayer.UserInterface.DisplayMessage("Zaczekaj aż przeciwnik ustawi pionki", MessageType.SideMessage);
+            } while (unitsToPlace.Any(selector));
         }
 
         public void Dispose() { }
